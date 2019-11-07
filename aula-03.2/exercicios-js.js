dfb99ee1547d8caea726cf9af672e18ec49fd75e
Erika2397 <erikabellidoalarcon@gmail.com>
2019-11-06 21:07:57 -0300

    JS exercises

---
+++ aula-03.2/exercicios-js.js
@@ -0,0 +1,11 @@
+let animes = ["Death Note", "Boku no hero", "Dragon Ball Z", "Cavaleiros do zodiaco", "Naruto", "Super Onze", "Nazo no canojo"];
+
+/*let i = 0
+while (i < animes.length){
+    console.log(animes[i])
+    i++;
+}*/
+
+for (i = 0; i < animes.length; i++){
+    console.log(animes[i]);
+}
