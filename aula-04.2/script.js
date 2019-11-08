ccdd81fe847e3af414ce8e49c9876eb636f8322b
Erika2397 <erikabellidoalarcon@gmail.com>
2019-11-08 19:24:02 -0300

    modificar javascript no template

---
+++ aula-04.2/script.js
@@ -0,0 +1,10 @@
+let botao = document.querySelector("button");
+
+/*function tirarBotao() {
+    if (botao.classList.contains("button")){
+        botao.classList.remove("button")
+    } else {
+        botao.classList.add("button");
+    }
+}*/
+
